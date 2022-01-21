2409dd79902b72a808ba828458c7546a55942d6f
Samuel Larsson <samuel@familjenlarsson.eu>
2022-01-21 15:00:25 +0100

    removed accidental console.log

--- util/hooks/useFormPersist.tsx
+++ util/hooks/useFormPersist.tsx
@@ -57,7 +57,6 @@ export default function useFormPersist({
 					const newHash = objectHash(persist.values);
 					if (hash !== newHash) {
 						setHash(newHash);
-						console.log(mutate);
 						mutate(persist.values);
 					}
 				}
