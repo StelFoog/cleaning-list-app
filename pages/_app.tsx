90c8f8211dfc48e1beb27974c828d5de30207478
Samuel Larsson <samuel@familjenlarsson.eu>
2022-01-21 17:34:52 +0100

    added loading indicator when switching page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,34 @@
 import '@fontsource/open-sans';
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
+import '../styles/nprogress.css';
 // Local imports
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import NProgress from 'nprogress';
 import ToastAlert from '../components/ToastAlert';
 import { SWRConfig } from 'swr';
 import { ResData } from '../types/db';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
 import useAuth from '../util/hooks/useAuth';
+import { useEffect } from 'react';
 
 function App({ Component, pageProps }: AppProps) {
 	const router = useRouter();
 	const [_, setAuth] = useAuth();
+
+	useEffect(() => {
+		router.events.on('routeChangeStart', NProgress.start);
+		router.events.on('routeChangeError', NProgress.done);
+		router.events.on('routeChangeComplete', NProgress.done);
+
+		return () => {
+			router.events.off('routeChangeStart', NProgress.start);
+			router.events.off('routeChangeError', NProgress.done);
+			router.events.off('routeChangeComplete', NProgress.done);
+		};
+	}, [router]);
 
 	return (
 		<ThemeProvider storageKey="cleaning::theme" enableSystem={false}>
