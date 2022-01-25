79a617b8f7781d5d50f6cb10dfa709e9cc2987a6
Samuel Larsson <samuel@familjenlarsson.eu>
2022-01-25 14:57:13 +0100

    imroved data fetching

--- util/hooks/useAxios.ts
+++ util/hooks/useAxios.ts
@@ -1,42 +1,34 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { useEffect } from 'react';
 import useSWR from 'swr';
 import useAuth from './useAuth';
 
 export default function useAxios<T = any>(url: string, options: AxiosRequestConfig = {}) {
 	const [auth] = useAuth();
-	const { data, error, mutate } = useSWR<T>(`/api/${url}`, async (url) => {
-		if (auth !== null) {
-			if (auth) {
-				return axios
-					.get(url, {
-						headers: { Authorization: auth },
-						timeout: 5000,
-						timeoutErrorMessage: "Couldn't get a response from the server",
-						...options,
-					})
-					.then((res: AxiosResponse<T>) => res.data)
-					.catch((e) => {
-						if (e.response) {
-							throw {
-								message: e.response.data.error,
-								deauthenticated: e.response.data.deauthenticated,
-							};
-						}
-						throw { message: e.message };
-					});
-			} else {
-				throw {
-					deauthenticated: true,
-				};
-			}
-		} else return new Promise((resolve) => resolve);
+	const { data, error } = useSWR<T>(auth !== null ? `/api/${url}` : null, async (url) => {
+		if (auth) {
+			return axios
+				.get(url, {
+					headers: { Authorization: auth },
+					timeout: 5000,
+					timeoutErrorMessage: "Couldn't get a response from the server",
+					...options,
+				})
+				.then((res: AxiosResponse<T>) => res.data)
+				.catch((e) => {
+					if (e.response) {
+						throw {
+							message: e.response.data.error,
+							deauthenticated: e.response.data.deauthenticated,
+						};
+					}
+					throw { message: e.message };
+				});
+		} else {
+			throw {
+				deauthenticated: true,
+			};
+		}
 	});
-
-	useEffect(() => {
-		if (auth) mutate();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [auth]);
 
 	const loading = auth === null || (!data && !error);
 
